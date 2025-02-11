4790ffd5f51d77fd5eb6675acaa1a89c46215e96
yasir aslam <yasir7942@outlook.com>
2025-02-11 10:15:19 +0500

    GMT and Meta pxil improve

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,7 @@ import { Open_Sans } from "next/font/google";
 import Footer from "./components/layout/footer";
 import siteConfig from "@/config/site";
 import Topbar from "./components/navigation/topbar";
-import GTMInitializer from "./components/elements/gmt";
-import MetaPixelInitializer from "./components/elements/MetaPixelInitializer";
+import Script from "next/script";
 
 
 
@@ -45,7 +44,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 
-
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
   const jsonLdBusiness = {
     "@context": "https://schema.org",
@@ -145,11 +145,71 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en"  >
 
+
+
+      <head>
+        {/* Google Tag Manager Script - Only load if GTM ID exists */}
+        {gtmId && (
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');
+            `}
+          </Script>
+        )}
+
+        {/* Meta Pixel (Facebook Pixel) */}
+        {metaPixelId && (
+          <Script id="fb-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              fbq('init', '${metaPixelId}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        )}
+
+      </head>
+
+
+
       <body className={openSans.className} >
 
-        <GTMInitializer /> {/* Initialize GTM here */}
-        <MetaPixelInitializer /> {/* Meta Pixel */}
 
+        {/* GTM NoScript Fallback */}
+        {gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+        )}
+
+        {/* Facebook NoScript Fallback */}
+        {metaPixelId && (
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            />
+          </noscript>
+        )}
 
 
         {/*  JSON-LD of Page */}
